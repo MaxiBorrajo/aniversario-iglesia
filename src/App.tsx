c9a44ed060ce880bd1db51f15842d2bbc29a0836
MaxiBorrajo <maximilianoborrajo0@gmail.com>
2025-04-11 17:34:12 -0300

    fix: adjust scroll logic to use window height for scrolling condition

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       if (!finished) {
         window.scrollTo(0, y);
         y += 1;
-        if (y < document.body.scrollHeight) {
+        if (y < window.innerHeight) {
           requestAnimationFrame(scrollDown);
         } else {
           setFinished(true);
